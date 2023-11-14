e02866805eb232895ecf0ac2b493ab8eb701e29e
LucasMarann <127116205+LucasMarann@users.noreply.github.com>
2023-11-13 21:33:16 -0300

    Update PaginaInicial.tsx

--- src/components/Pages/PaginaInicial.tsx
+++ src/components/Pages/PaginaInicial.tsx
@@ -1,31 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from "react-native-vector-icons/FontAwesome";
 import axios from "axios";
 import List from "../PagesComponents/List";
-import styles from "../PagesStyle/TelaPaginaInicial"; 
+import styles from "../PagesStyle/TelaPaginaInicial";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const PaginaInicial = ({ navigation }) => {
-  const [userData, setUserData] = useState({});
+  const [listPets, setListPets] = useState({});
   const [menuVisible, setMenuVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const listarPets = async () => {
+    const token = await AsyncStorage.getItem("token");
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        "https://tamagochiapi-clpsampedro.b4a.run/pets"
+      );
+      setLoading(false);
+      setListPets(response.data?.pets || []);
+    } catch (error) {
+      console.error("Erro na requisição da API", error);
+    }
+  };
 
   useEffect(() => {
-      async function fetchData() {
-        const token = await AsyncStorage.getItem("token");
-      try {
-        const response = await axios.get("https://tamagochiapi-clpsampedro.b4a.run/pets", 
-        {
-            headers: {
-                "x-access-token": token,
-        }});
-        setUserData(response);
-      } catch (error) {
-        console.error("Erro na requisição da API", error);
-      }
-    }
-
-    fetchData();
+    listarPets();
   }, []);
 
   const toggleMenu = () => {
@@ -33,31 +34,30 @@ const PaginaInicial = ({ navigation }) => {
   };
 
   const handleLogout = async () => {
-        await AsyncStorage.removeItem("token");
-        navigation.navigate("Login");
-    };
+    await AsyncStorage.removeItem("token");
+    navigation.navigate("Login");
+  };
 
   return (
     <View style={styles.container}>
-        <View style={styles.header}>
-            <TouchableOpacity onPress={toggleMenu}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={toggleMenu}>
+          <Icon name="user" style={styles.userIcon} />
+        </TouchableOpacity>
+        {menuVisible && (
+          <View style={styles.userMenu}>
             <Icon name="user" style={styles.userIcon} />
+            <Text style={styles.userName}>Nome do usuario</Text>
+            <TouchableOpacity onPress={handleLogout}>
+              <Text style={styles.logoutButton}>Logout</Text>
             </TouchableOpacity>
-            {menuVisible && (
-            <View style={styles.userMenu}>
-                <Icon name="user" style={styles.userIcon} />
-                <Text style={styles.userName}>Nome do usuario</Text>
-                <TouchableOpacity onPress={handleLogout}>
-                <Text style={styles.logoutButton}>Logout</Text>
-                </TouchableOpacity>
-            </View>
-            )}
-        </View>
-        <View >
-            <List data = {userData}/>
-        </View>
+          </View>
+        )}
+      </View>
+      <View>
+        <List data={listPets} />
+      </View>
     </View>
-
   );
 };
 
