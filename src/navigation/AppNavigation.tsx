ee742a8428ce22795c71e876297f59033565dd8f
LucasMarann <127116205+LucasMarann@users.noreply.github.com>
2023-11-14 08:05:53 -0300

    Update AppNavigation.tsx

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from '../components/Pages/Home';
-import Login from '../components/Pages/Login';
-import Cadastro from '../components/Pages/Cadastro';
-import PaginaInicial from '../components/Pages/PaginaInicial';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Home from "../components/Pages/Home";
+import Login from "../components/Pages/Login";
+import Cadastro from "../components/Pages/Cadastro";
+import PaginaInicial from "../components/Pages/PaginaInicial";
+import Game from "../components/Pages/Game";
 
 const Stack = createNativeStackNavigator();
+
+axios.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers["x-access-token"] = token;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
+        {/* <Stack.Screen name="Cobra" component={Game} /> */}
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Cadastro" component={Cadastro} />
         <Stack.Screen name="PaginaInicial" component={PaginaInicial} />
