d5d97f378894ebc53e10869307fab6ce6ddba048
LucasMarann <127116205+LucasMarann@users.noreply.github.com>
2023-11-14 21:36:43 -0300

    Update PaginaInicial.tsx

--- src/components/Pages/PaginaInicial.tsx
+++ src/components/Pages/PaginaInicial.tsx
@@ -1,68 +1,50 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { View, Text, TouchableOpacity, ImageBackground } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
-import axios from "axios";
-import List from "../PagesComponents/List";
-import styles from "../PagesStyle/TelaPaginaInicial";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import styles from "../PagesStyle/TelaPaginaInicial";
 
 const PaginaInicial = ({ navigation }) => {
-  const [listPets, setListPets] = useState({});
-  const [menuVisible, setMenuVisible] = useState(false);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    listarPets();
-  }, []);
-
-  const listarPets = async () => {
-    setLoading(true);
-    try {
-      const response = await axios.get(
-        "https://tamagochiapi-clpsampedro.b4a.run/pets"
-      );
-      setLoading(false);
-      console.log("Lista de pets:", response);
-      // setListPets(response.data?.pets || []);
-    } catch (error) {
-      console.error("Erro na requisição da API", error);
-    }
-  };
-
-  const toggleMenu = () => {
-    setMenuVisible(!menuVisible);
-  };
-
   const handleLogout = async () => {
     await AsyncStorage.removeItem("token");
+    console.log("Logout realizado com sucesso!");
     navigation.navigate("Login");
   };
 
-  const playGame = () => {
-    navigation.navigate("Cobra");
-  };
-
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity onPress={toggleMenu}>
-          <Icon name="user" style={styles.userIcon} />
+    <ImageBackground
+      source={require("../../assets/Imagem5.jpg")}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+          <Icon name="sign-out" size={30} color="black" />
         </TouchableOpacity>
-        {menuVisible && (
-          <View style={styles.userMenu}>
-            <Icon name="user" style={styles.userIcon} />
-            <Text style={styles.userName}>Nome do usuario</Text>
-            <TouchableOpacity onPress={playGame}>
-              <Text style={styles.userName}>Jogar</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={handleLogout}>
-              <Text style={styles.logoutButton}>Logout</Text>
-            </TouchableOpacity>
-          </View>
-        )}
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate("ListarPets")}
+          >
+            <Text style={styles.buttonText}>Listar Pets</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate("CadastroPets")}
+          >
+            <Text style={styles.buttonText}>Cadastrar Pets</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate("Cobra")}
+          >
+            <Text style={styles.buttonText}>Cobrinha</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <View>{/* <List data={listPets} /> */}</View>
-    </View>
+    </ImageBackground>
   );
 };
 
